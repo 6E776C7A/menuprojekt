8bfe26c185a1e65ef28972e59e41d6b4e39a1265
PajerskiPatryk <patpaj@student.agh.edu.pl>
2025-01-04 15:07:50 +0100

    1

--- xd.py
+++ xd.py
@@ -2,14 +2,14 @@ from geopy.distance import geodesic
 import gpxpy
 
 # Ścieżki plików
-input_gpx_path = "data/pomiar_lekkie_zaklucenia.gpx"  # Ścieżka do pliku wejściowego
-output_gpx_path = "data/pomiar_lekkie_zaklucenia_updated.gpx"  # Ścieżka do pliku wyjściowego
+input_gpx_path = "data/pomiar_bez_zaklucen.gpx"  # Ścieżka do pliku wejściowego
+output_gpx_path = "data/pomiar_bez_zaklucen_updated.gpx"  # Ścieżka do pliku wyjściowego
 
 # Punkty skrajne (start i koniec) z wysokościami
 start_coords = (49 + 25/60 + 37/3600, 19 + 50/60 + 30/3600)  # (49°25'37"N, 19°50'30"E)
-end_coords = (49 + 26/60 + 26/3600, 19 + 51/60 + 7/3600)     # (49°26'26"N, 19°51'07"E)
+end_coords = (49 + 26/60 + 28/3600, 19 + 51/60 + 7/3600)     # (49°26'26"N, 19°51'07"E)
 start_ele = 691  # Wysokość w metrach
-end_ele = 676    # Wysokość w metrach
+end_ele = 675    # Wysokość w metrach
 
 # Wczytaj plik GPX
 with open(input_gpx_path, 'r') as gpx_file:
